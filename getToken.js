c09aaa6fffa804d9d871a8dc8d4d1c7283590e34
bossolan <bossolan@hotmail.com>
2021-03-21 09:40:36 -0300

    ok

---
+++ getToken.js
@@ -0,0 +1,57 @@
+function getToken(req, res){
+
+    console.log(req.query.code);
+
+    const code = req.query.code
+
+    const axios = require('axios')
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'authorization_code');
+    params.append('client_id', '8909978435931711');
+    params.append('client_secret', 'v6GbWGKASUGmarVRaHIMJ3WCRdjetfNZ');
+    params.append('code', code);
+    params.append('redirect_uri', 'https://mlivre.herokuapp.com/home');
+
+    axios.get('https://auth.mercadolibre.com.ar/oauth/token', params)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        console.log('done');
+      });  
+
+
+    /*curl -X POST \
+    -H 'accept: application/json' \
+    -H 'content-type: application/x-www-form-urlencoded' \
+    'https://api.mercadolibre.com/oauth/token' \
+    -d 'grant_type=authorization_code' \
+    -d 'client_id=$APP_ID' \
+    -d 'client_secret=$SECRET_KEY' \
+    -d 'code=$SERVER_GENERATED_AUTHORIZATION_CODE' \
+    -d 'redirect_uri=$REDIRECT_URI'*/
+
+    /*
+    const clienteID = '8909978435931711'    
+    const redirectURI = 'https://mlivre.herokuapp.com/home'
+
+    const secretKey = 'v6GbWGKASUGmarVRaHIMJ3WCRdjetfNZ'
+    const url = 'http://auth.mercadolivre.com.br/authorization'
+
+    const x = 'https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=' + clienteID + '&redirect_uri=' + redirectURI
+    */
+
+}
+
+
+module.exports = {
+    getToken: getToken
+};
+
+
+
+ 
