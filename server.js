3ed37468f4b74fbb5341c91f3b5ebe988d8abae5
bossolan <bossolan@hotmail.com>
2021-03-22 09:01:18 -0300

    ok

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ app.get('/home', (req, res) => { getToken.getToken(req, res, app) })
 app.get('/tokenCode', (req, res) => { getTokenCode.getTokenCode(req, res) })
 
 var path = require('path');
+
+process.env.PWD = process.cwd();
+app.use(express.static(path.join(process.env.PWD, 'public')));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/pljml.html'));
 });
 
-app.listen(process.env.PORT, () =>{
-    console.log('App listening on port ' + process.env.PORT);
+app.listen(process.env.PORT || 3030, () =>{
+    console.log('App listening on port ' + (process.env.PORT || 3030));
 })
